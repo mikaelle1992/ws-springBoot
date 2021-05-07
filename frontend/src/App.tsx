853e222f30e0c6a0dab6aa8f51b26656666768f5
mikaelle rubia <mikaellerubiaps02@gmail.com>
2021-05-07 11:57:58 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,39 @@
-import NavBar from  'assets/components/Navbar';
-import Footer from  'assets/components/Footer';
-import  DataTable from  'assets/components/DataTable';
+import NavBar from 'assets/components/Navbar';
+import Footer from 'assets/components/Footer';
+import DataTable from 'assets/components/DataTable';
+import BarChart from 'assets/components/BarChart';
+import DonutChart from 'assets/components/DonutChart';
 
 function App() {
   return (
     <>
-    <NavBar />
-    <div className ="cantainer ">  
-     <h1 className = "text-primary">Olá mundo ! </h1>  
+      <NavBar />
+      <div className="container ">
+        <h1 className="text-primary">Dashboard de Vendas </h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Todas as Vendas</h5>
+            <DonutChart />
+          </div>          
+        </div>
+
+
+          <div className ="py-3">
+          <h2 className ="text-primary"> Todas as Vendas </h2>
+          </div>
+
         <DataTable />
-    </div>
-   
-    <Footer/>
+      </div>
+
+      <Footer />
     </>
   );
 }
 
-export default App; 
+export default App;
